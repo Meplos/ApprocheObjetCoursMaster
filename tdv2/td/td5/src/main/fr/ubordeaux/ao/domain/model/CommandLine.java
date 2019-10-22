d0a09a0fd2af17f34da3e7e0b2b54eedec20176c
Alexandre Erard <aerard@emi.u-bordeaux1.fr>
2019-10-22 10:04:38 +0200

    commanDline

--- tdv2/td/td5/src/main/fr/ubordeaux/ao/domain/model/CommandLine.java
+++ tdv2/td/td5/src/main/fr/ubordeaux/ao/domain/model/CommandLine.java
@@ -1,15 +1,17 @@
 package fr.ubordeaux.ao.domain.model;
 
-public class CommanLine {
+public class CommandLine {
     private Reference reference;
     private PosInt quantity;
     private PosInt price;
 
-    public CommanLine(Reference ref, PosInt quantity, PosInt price) {
+    public CommandLine(Reference ref, PosInt quantity, PosInt price) {
         this.reference = ref;
         this.quantity = quantity;
         this.price = price;
     }
+
+    
 
     
 }
